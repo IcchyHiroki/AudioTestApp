dfae2601a18eea8b08ad61e2f51e33350bc31aa8
IcchyHiroki <icchyWorks@gmail.com>
2022-07-27 15:16:35 +0900

    Initial Commit

---
+++ AudioTestApp/AudioTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioTestAppApp.swift
+//  AudioTestApp
+//
+//  Created by Hiroki Ichikura on 2022/07/27.
+//
+
+import SwiftUI
+
+@main
+struct AudioTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
